bb0d40c2769f3ca932d287f7fe99cdccf0e4dba9
mashuk06 <mushfiqur.rahman06@gmail.com>
2023-07-09 19:52:30 +0600

    update code to ES6 structure

--- module-2-exercises.js
+++ module-2-exercises.js
@@ -1,11 +1,11 @@
 
 //Exercise 1:
 
-function destructureExample(obj, arr) {
-  const { name, age } = obj
-  const [value0, , value2] = arr
+const destructureExample = (obj, arr) => {
+  const { name, age } = obj;
+  const [value0, , value2] = arr;
 
-  return { name, age }
+  return { name, age };
 }
 
 const obj = { name: 'John', age: 30, city: 'New York' }
@@ -18,9 +18,7 @@ console.log(result1)
 //output: { name: 'John', age: 30 }
 
 //Exercise 2
-function sumNumbers(...numbers) {
-  return numbers.reduce((sum, number) => sum + number, 0)
-}
+const sumNumbers = (...numbers) => numbers.reduce((sum, number) => sum + number, 0)
 
 const result2 = sumNumbers(1, 2, 3, 4, 5)
 consoleLog(2,result2)
@@ -28,9 +26,7 @@ consoleLog(2,result2)
 //output: 15
 
 //Exercise 3
-function createGreeting(name) {
-  return `Hello, ${name}! Welcome to our website.`
-}
+const createGreeting = name => `Hello, ${name}! Welcome to our website.`
 
 const result3 = createGreeting('Alice')
 consoleLog(3,result3)
@@ -38,9 +34,7 @@ consoleLog(3,result3)
 //output: Hello, Alice! Welcome to our website.
 
 //Exercise 4
-function isEven(number) {
-  return number % 2 === 0 ? 'Even' : 'Odd'
-}
+const isEven = number => (number % 2 === 0 ? 'Even' : 'Odd')
 
 const result4 = isEven(7)
 consoleLog(4,result4)
@@ -51,9 +45,7 @@ consoleLog(4,result4)
 const multiply = (a, b) => a * b
 
 //Exercise 6
-function getLargestNumber(a, b) {
-  return a > b ? a : b
-}
+const getLargestNumber = (a, b) => (a > b ? a : b)
 
 const result5 = getLargestNumber(10, 5)
 consoleLog(6,result5)
@@ -61,9 +53,7 @@ consoleLog(6,result5)
 //Output: 10
 
 //Exercise 7
-function getAddressCity(address) {
-  return address?.city ?? 'Unknown'
-}
+const getAddressCity = address => address?.city ?? 'Unknown'
 
 const address = { street: '123 Main St', country: 'USA' }
 const result6 = getAddressCity(address)
@@ -72,9 +62,7 @@ consoleLog(7,result6)
 //Output: Unknown
 
 //Exercise 8
-function doubleNumbers(numbers) {
-  return numbers.map(number => number * 2)
-}
+const doubleNumbers = numbers => numbers.map(number => number * 2)
 
 const result7 = doubleNumbers([1, 2, 3, 4, 5])
 consoleLog(8,result7)
@@ -82,9 +70,7 @@ consoleLog(8,result7)
 //Output: [ 2, 4, 6, 8, 10 ]
 
 //Exercise 9
-function filterEvenNumbers(numbers) {
-  return numbers.filter(number => number % 2 === 0)
-}
+const filterEvenNumbers = numbers => numbers.filter(number => number % 2 === 0)
 
 const result8 = filterEvenNumbers([1, 2, 3, 4, 5])
 consoleLog(9,result8)
@@ -92,19 +78,24 @@ consoleLog(9,result8)
 //Output: [2, 4]
 
 //Exercise 10
-function sumArray(numbers) {
-  return numbers.reduce((sum, number) => sum + number, 0)
-}
+const sumArray = numbers => numbers.reduce((sum, number) => sum + number, 0)
 
 const result9 = sumArray([1, 2, 3, 4, 5])
 consoleLog(10,result9)
 
 //Output: 15
 
+//Exercise 11
+const sortNumbers = numbers => numbers.sort((a, b) => a - b)
+
+const result10 = sortNumbers([5, 2, 8, 1, 4])
+consoleLog(11,result10)
+
+//Output: [1, 2, 4, 5, 8]
+
+
+//common function for log data
 function consoleLog(exerciseNo,result) {
   console.log(`Exercise ${exerciseNo} result: ` + result)
 }
-
-
-
 
